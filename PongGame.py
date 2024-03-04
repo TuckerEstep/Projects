969807d269557a308131d367f75bc36dec179f0d
TuckerEstep <91077575+TuckerEstep@users.noreply.github.com>
2024-03-04 16:12:06 -0600

    Update PongGame.py

--- PongGame.py
+++ PongGame.py
@@ -1,16 +1,12 @@
 import pygame
 pygame.init()
 
-
 WIDTH, HEIGHT = 700, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pong")
-
-
 FPS = 60
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-
 PADDLE_WIDTH, PADDLE_HEIGHT = 20, 100
 BALL_RADIUS = 7
 
@@ -59,8 +55,6 @@ class Ball:
         self.y_vel = 0
         self.x_vel *= 1
 
-
-
 def draw(win, paddles, ball, left_score, right_score):
     win.fill(BLACK)
 
@@ -89,7 +83,6 @@ def handle_collision(ball, left_paddle, right_paddle):
         if ball.y >= left_paddle.y and ball.y <= left_paddle.y + left_paddle.height:
             if ball.x - ball.radius <= left_paddle.x + left_paddle.width:
                 ball.x_vel *= -1
-
                 middle_y = left_paddle.y + left_paddle.height / 2
                 difference_in_y = middle_y - ball.y
                 reduction_factor = (left_paddle.height / 2) / ball.MAX_VEL
@@ -99,22 +92,22 @@ def handle_collision(ball, left_paddle, right_paddle):
         if ball.y >= right_paddle.y and ball.y <= right_paddle.y + right_paddle.height:
             if ball.x + ball.radius >= right_paddle.x:
                 ball.x_vel *= -1
-
                 middle_y = right_paddle.y + right_paddle.height / 2
                 difference_in_y = middle_y - ball.y
                 reduction_factor = (right_paddle.height / 2) / ball.MAX_VEL
                 y_vel = difference_in_y / reduction_factor
                 ball.y_vel = -1 * y_vel
 
-
 def handle_paddle_movement(keys, left_paddle, right_paddle):
     if keys[pygame.K_w] and left_paddle.y - left_paddle.VEL >= 0:
         left_paddle.move(up=True)
+        
     if keys[pygame.K_s] and left_paddle.y + left_paddle.VEL + left_paddle.height <= HEIGHT:
         left_paddle.move(up=False)
 
     if keys[pygame.K_UP] and right_paddle.y - right_paddle.VEL >= 0:
         right_paddle.move(up=True)
+        
     if keys[pygame.K_DOWN] and right_paddle.y + right_paddle.VEL + right_paddle.height <= HEIGHT:
         right_paddle.move(up=False)
 
@@ -125,7 +118,6 @@ def main():
     left_paddle = Paddle(10, HEIGHT//2 - PADDLE_WIDTH//2, PADDLE_WIDTH, PADDLE_HEIGHT)
     right_paddle = Paddle(WIDTH - 10 - PADDLE_WIDTH, HEIGHT // 2 - PADDLE_WIDTH // 2, PADDLE_WIDTH, PADDLE_HEIGHT)
     ball = Ball(WIDTH//2, HEIGHT//2, BALL_RADIUS)
-
     left_score = 0
     right_score = 0
 
@@ -136,12 +128,14 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
                 break
+                
         keys = pygame.key.get_pressed()
         handle_paddle_movement(keys, left_paddle, right_paddle)
 
         if ball.x < 0:
             right_score += 1
             ball.reset()
+            
         elif ball.x > WIDTH:
             left_score += 1
             ball.reset()
